c39b2a2a8d89122d7269bf5620e88ab35d11592c
Joel Palmer <joel@Joels-iMac.local>
2018-08-10 15:14:19 -0500

    minor code style updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ export default class Form extends Component {
   render() {
     return (
       <div className="Form">
-        <form onSubmit={(e) => this.props.handleSubmit(e)}>
+        <form onSubmit={e => this.props.handleSubmit(e)}>
           <input
             type="text"
             value={this.props.value}
--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ export default class Task extends Component {
     return (
       <div className="Task">
         <span style={{ textDecoration: this.props.todo.done ? 'line-through' : 'none'}}> {this.props.todo.value} </span> 
-        <button onClick={() =>this.props.handleClick(this.props.index)}>{this.props.todo.done ? 'Undo' : 'Complete'}</button>
+        <button onClick={() => this.props.handleClick(this.props.index)}>{this.props.todo.done ? 'Undo' : 'Complete'}</button>
       </div>
     );
   }
